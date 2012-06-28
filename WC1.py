7ab366817d5c4af4ccc17d9cc3b8ef000dd63be2
wlee <tree03@paran.com>
2012-06-28 16:45:38 -0500

    schema v2 validated

--- WC1.py
+++ WC1.py
@@ -1,6 +1,10 @@
+import xml.etree.ElementTree as ET
+
 import wsgiref.handlers
 
 from google.appengine.ext import webapp
+
+
 
 class MainHandler(webapp.RequestHandler):
   def get(self):
@@ -12,6 +16,40 @@ class MainHandler(webapp.RequestHandler):
     
     self.response.out.write(outstr)
 
+def read(filename):
+  tree = ET.ElementTree(file='WC_orig.xml')
+  #tree = ET.parse(filename)
+  root = tree.getroot()
+  
+  #f = open("wc.out", 'w')
+  
+  for child_of_root in root:
+    print child_of_root.tag, child_of_root.attrib
+  
+  tree = ET.parse('WC_orig.xml')
+  
+  
+  
+  #print "read complete"
+  
+  
+  #children = root.getchildren()
+  
+  #f = open("wc.out", 'w')
+  #f.write(elem)
+  #w = "wc.out"
+  #w.write(' '.join([str(tree[k]) for k in xrange(lenTree)]))
+  
+  #elem = tree.getroot()
+
 def main():
+  read("WC.xml")
   application = webapp.WSGIApplication([('/', MainHandler)], debug=True)
   wsgiref.handlers.CGIHandler().run(application)
+
+
+"""
+to run:
+google_appengine/dev_appserver.py cs373-wc/
+http://localhost:8080/
+"""
