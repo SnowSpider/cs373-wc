7fc3abbb16789e824a9f50392084739a2438d6bb
sogol <sogol@utexas.edu>
2012-06-28 18:30:03 -0500

    improved import's skeleton

--- WC1.py
+++ WC1.py
@@ -2,6 +2,60 @@ import wsgiref.handlers
 import xml.etree.cElementTree as ET
 from google.appengine.ext import webapp
 import logging
+from google.appengine.ext import db
+from google.appengine.ext.db import polymodel
+
+class ContactInfo(db.Model):
+    phone_number = PhoneNumberProperty
+    email = EmailProperty()
+    address = PostalAddressProperty()
+
+class Person(db.Model):
+    name = db.StringProperty(required=True)
+    kind = db.StringProperty()
+    location = db.StringProperty()
+    history = db.StringProperty()
+    images = db.ListProperty(db.Link)
+    videos = db.ListProperty(db.Link)
+    social_networks = db.ListProperty(db.Link)
+    external_links = db.ListProperty(db.Link)
+    related_crises = db.ListProperty(str)
+    related_orgs = db.ListProperty(str)
+    
+class Organization(db.Model):
+    name = db.StringProperty(required=True)
+    kind = db.StringProperty()
+    location = db.StringProperty()
+    history = db.StringProperty()
+    contact_info = ContactInfo()
+    images = db.ListProperty(db.Link)
+    videos = db.ListProperty(db.Link)
+    social_networks = db.ListProperty(db.Link)
+    external_links = db.ListProperty(db.Link)
+    related_crises = db.ListProperty(str)
+    related_people = db.ListProperty(str)
+    
+class Crisis(db.Model):
+    name = db.StringProperty(required=True)
+    kind = db.StringProperty()
+    location = db.StringProperty()
+    date_and_time = db.StringProperty()
+    human_impact = db.StringProperty()
+    economic_impact = db.StringProperty()
+    resources_needed = db.StringProperty()
+    ways_to_help = db.StringProperty()
+    history = db.StringProperty()
+    images = db.ListProperty(db.Link)
+    videos = db.ListProperty(db.Link)
+    social_networks = db.ListProperty(db.Link)
+    external_links = db.ListProperty(db.Link)
+    related_orgs = db.ListProperty(str)
+    related_people = db.ListProperty(str)
+
+class Everything(db.Model):
+    people = db.ListProperty(Person)
+    crises = db.ListProperty(Crisis)
+    organizations = db.ListProperty(Organization)
 
 class MainHandler(webapp.RequestHandler):
   def get(self):
@@ -14,13 +68,17 @@ class MainHandler(webapp.RequestHandler):
     self.response.out.write(outstr)
 
 
+#Assumes valid xml instance
 def ImportXml(filename):
   tree = ET.parse(filename)
-  node = tree.find('./with_attributes')
-  logging.debug( node.tag)
-  for name, value in sorted(node.attrib.items()):
-      logging.debug((name,value))
-  logging.debug("")   
+  root = tree.getroot()
+  logging.debug(root)
+  logging.debug("HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII")
+  people = root.find("people").findall("person")
+  logging.debug(people)
+  for person in people :
+    person_model = Person()
+    person_model.name = person.find("name")
 
 def main():
   
