faac8f7981a157461c81fb5e184ba6317909e285
sogol <sogol@utexas.edu>
2012-06-28 17:19:31 -0500

    added import skeleton

--- WC1.py
+++ WC1.py
@@ -1,10 +1,7 @@
-import xml.etree.ElementTree as ET
-
 import wsgiref.handlers
-
+import xml.etree.cElementTree as ET
 from google.appengine.ext import webapp
-
-
+import logging
 
 class MainHandler(webapp.RequestHandler):
   def get(self):
@@ -13,43 +10,19 @@ class MainHandler(webapp.RequestHandler):
     inFile = open("htmlgoodies/mockup.html", 'r')
     outstr = inFile.read() #"HELLO CAR RAMROD"
     inFile.close()
-    
+    ImportXml("WC.xml")
     self.response.out.write(outstr)
 
-def read(filename):
-  tree = ET.ElementTree(file='WC_orig.xml')
-  #tree = ET.parse(filename)
-  root = tree.getroot()
-  
-  #f = open("wc.out", 'w')
-  
-  for child_of_root in root:
-    print child_of_root.tag, child_of_root.attrib
-  
-  tree = ET.parse('WC_orig.xml')
-  
-  
-  
-  #print "read complete"
-  
-  
-  #children = root.getchildren()
-  
-  #f = open("wc.out", 'w')
-  #f.write(elem)
-  #w = "wc.out"
-  #w.write(' '.join([str(tree[k]) for k in xrange(lenTree)]))
-  
-  #elem = tree.getroot()
+
+def ImportXml(filename):
+  tree = ET.parse(filename)
+  node = tree.find('./with_attributes')
+  logging.debug( node.tag)
+  for name, value in sorted(node.attrib.items()):
+      logging.debug((name,value))
+  logging.debug("")   
 
 def main():
-  read("WC.xml")
+  
   application = webapp.WSGIApplication([('/', MainHandler)], debug=True)
   wsgiref.handlers.CGIHandler().run(application)
-
-
-"""
-to run:
-google_appengine/dev_appserver.py cs373-wc/
-http://localhost:8080/
-"""
