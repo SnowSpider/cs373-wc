1e0920d624b5fb256d6f79836d091f08239c01dd
smoshtaghi <sogol@utexas.edu>
2012-07-30 13:31:32 -0500

    3 tests for merge done

--- test/TestWC3.py
+++ test/TestWC3.py
@@ -28,7 +28,7 @@ import unittest
 import logging
 
 
-from WC3 import ImportXml, ExportXml, fixAmp, debug, trim, xstr, xint, merge, Date, HumanImpact, Location, EconomicImpact, Impact, Link, Reference, Crisis, CrisisInfo
+from WC3 import ImportXml, ExportXml, fixAmp, debug, trim, xstr, xint, merge, Date, HumanImpact, Location, EconomicImpact, Impact, Link, Reference, Crisis, CrisisInfo, FullAddress, ContactInfo, OrgInfo, Organization, PersonInfo, Person
 
 # -------
 # TestWC3
@@ -112,6 +112,146 @@ class TestWC3 (unittest.TestCase) :
             if crisis.find("name").text == "Libyan Civil War":
                  merge(entity, crisis)
         self.assert_(entity.info.time.year == 2011)
+        
+        
+        
+        
+        
+    def test_ImportXml_2 (self) :
+    	
+        myMail = FullAddress(address = None,
+        				     city = None,
+        				     state = None,
+        				     country = None,
+        				     zip_ = None)
+        
+       
+        myMail.put()
+        					
+        
+        myContact = ContactInfo(Phone = None,
+                     email =None,
+                     mail = myMail)
+                     
+        myContact.put()
+        
+        myLoc = Location(city = None,
+                            region = None,
+                            country = None,
+                            missing = None) 
+        myLoc.put()
+                           
+        
+        myInfo = OrgInfo(type_ = None,
+                            history = None,
+          					contact = myContact,
+                            loc = myLoc
+                            )
+        myInfo.put()
+        
+        myLink = Link(site = None,
+                     title = None,
+                     url = "https://www.google.com",
+                     description = None)
+        myLink.put()
+        
+        myReference = Reference(primaryimage = myLink.key())
+        myReference.images.append(myLink.key())
+        myReference.videos.append(myLink.key())
+        myReference.socials.append(myLink.key())
+        myReference.exts.append(myLink.key())
+        
+        myReference.put()
+        
+        
+        entity = Organization(key_name = "International_Criminal_Court", 
+                        idref = "International_Criminal_Court", 
+                        name = "International Criminal Court",
+                        info = myInfo,
+                        ref = myReference,
+                        misc = None,
+                        relatedCrises = [],
+                        relatedPeople = []
+                        )
+                        
+        
+                      
+        
+        
+        tree = ET.ElementTree(file = open("WC2.xml", "r"))
+        root = tree.getroot()
+        orgs = root.findall("organization")
+        for organization in orgs: 
+            if organization.find("name").text == "International Criminal Court":
+                 merge(entity, organization)
+        self.assert_(entity.info.loc.city == "Hague")
+        
+        
+        
+        
+        
+        
+        
+    def test_ImportXml_3 (self) :
+        					
+        
+
+
+        
+        myBirthdate = Date(time = None,
+        				   day = 0,
+        				   month = 0,
+        				   year = 0,
+        				   misc = None)
+        myBirthdate.put()
+        				   
+                           
+        
+        myInfo = PersonInfo(type_ = None,
+                            birthdate = myBirthdate,
+          					nationality = None,
+                            biography = None
+                            )
+        myInfo.put()
+        
+        myLink = Link(site = None,
+                     title = None,
+                     url = "https://www.google.com",
+                     description = None)
+        myLink.put()
+        
+
+        myReference = Reference(primaryimage = myLink.key())
+        myReference.images.append(myLink.key())
+        myReference.videos.append(myLink.key())
+        myReference.socials.append(myLink.key())
+        myReference.exts.append(myLink.key())
+        
+        myReference.put()
+        
+        
+        entity = Person(key_name = "Mir_Hossein_Mousavi", 
+                        idref = "Mir_Hossein_Mousavi", 
+                        name = "Mir Hossein Mousavi",
+                        info = myInfo,
+                        ref = myReference, #TODO
+                        misc = None,
+                        relatedCrises = [],
+                        relatedOrgs = []
+                        )
+                        
+        
+                      
+        
+        
+        tree = ET.ElementTree(file = open("WC2.xml", "r"))
+        root = tree.getroot()
+        people = root.findall("person")
+        for person in people: 
+            if person.find("name").text == "Mir Hossein Mousavi":
+                 merge(entity, person)
+        self.assert_(entity.info.nationality == "Iran")
+        
         
         
         
